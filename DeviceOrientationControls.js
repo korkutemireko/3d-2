f86b409ce5a280d37fcb144b83581a4df4df80d3
korkutemir <korkutemir27@gmail.com>
2023-08-13 14:45:18 +0300

    Update DeviceOrientationControls.js

--- DeviceOrientationControls.js
+++ DeviceOrientationControls.js
@@ -1,4 +1,4 @@
- import * as THREE from "https://unpkg.com/browse/three@0.155.0/build/three.module.js";
+ import * as THREE from "https://cdn.skypack.dev/three@0.136.0";
 
 THREE.DeviceOrientationControls = function ( camera ) {
 
